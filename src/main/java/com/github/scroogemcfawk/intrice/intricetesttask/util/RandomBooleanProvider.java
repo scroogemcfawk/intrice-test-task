46e71972569c4caaf8cae67ac49334a7392ed7bc
scroogemcfawk <scroogemcfawk@gmail.com>
2023-12-24 15:51:35 +0700

    add readme.md

--- src/main/java/com/github/scroogemcfawk/intrice/intricetesttask/util/RandomBooleanProvider.java
+++ src/main/java/com/github/scroogemcfawk/intrice/intricetesttask/util/RandomBooleanProvider.java
@@ -3,10 +3,6 @@ package com.github.scroogemcfawk.intrice.intricetesttask.util;
 
 /**
  * This class implements RandomBooleanProvider interface.
- * It's made in order to decouple OutcomeService from a class that provides random methods.
- * The reason is that it's supposed to be used with gambling-like system, so pseudo-random
- * might bring vulnerabilities to it, therefore implementation can be easily changed with
- * no need to change the code that depends on it.
  */
 public interface RandomBooleanProvider
 {
