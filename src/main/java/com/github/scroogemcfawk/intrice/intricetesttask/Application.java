451a001a0ea7ba0bb3ad5cc6a196b5aebc119d6b
scroogemcfawk <scroogemcfawk@gmail.com>
2023-12-24 12:51:43 +0700

    disable DB auto configuraiton

--- src/main/java/com/github/scroogemcfawk/intrice/intricetesttask/Application.java
+++ src/main/java/com/github/scroogemcfawk/intrice/intricetesttask/Application.java
@@ -2,8 +2,19 @@ package com.github.scroogemcfawk.intrice.intricetesttask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(
+        // DB is not needed in this project
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                DataSourceTransactionManagerAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 public class Application
 {
     public static void main(String[] args)
